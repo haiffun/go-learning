a29ef1df20187f5a5a1fd04c55d9de40087b8315
haif <1623302021@qq.com>
2021-03-07 17:44:53 +0800

    format code

--- src/basic/basic_data_type.go
+++ src/basic/basic_data_type.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"math/cmplx"
+    "fmt"
+    "math/cmplx"
 )
 
 var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
+    ToBe   bool       = false
+    MaxInt uint64     = 1<<64 - 1
+    z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 const (
-	Big   = 1 << 100
-	Small = Big >> 99
+    Big   = 1 << 100
+    Small = Big >> 99
 )
 
 // 基本数据类型
 func main() {
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T Value: %v\n", z, z)
+    fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+    fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+    fmt.Printf("Type: %T Value: %v\n", z, z)
 
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+    var i int
+    var f float64
+    var b bool
+    var s string
+    fmt.Printf("%v %v %v %q\n", i, f, b, s)
 
-	//var x int = 42
-	//var y float64 = float64(x)
-	//var u uint = uint(y)
+    //var x int = 42
+    //var y float64 = float64(x)
+    //var u uint = uint(y)
 
-	x := 42 // 类型推导
-	y := float64(x)
-	u := uint(y)
-	fmt.Printf("%T %v %T %v %T %v\n", x, x, y, y, u, u)
+    x := 42 // 类型推导
+    y := float64(x)
+    u := uint(y)
+    fmt.Printf("%T %v %T %v %T %v\n", x, x, y, y, u, u)
 
-	const Pi = 3.14 // 常量
-	fmt.Println(Pi)
+    const Pi = 3.14 // 常量
+    fmt.Println(Pi)
 
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+    fmt.Println(needInt(Small))
+    fmt.Println(needFloat(Small))
+    fmt.Println(needFloat(Big))
 }
 
 func needInt(x int) int {
-	return x*10 + 1
+    return x*10 + 1
 }
 
 func needFloat(x float64) float64 {
-	return x * 0.1
+    return x * 0.1
 }
--- src/basic/basic_grammar.go
+++ src/basic/basic_grammar.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"math/rand"
-	"time"
+    "fmt"
+    "math"
+    "math/rand"
+    "time"
 )
 
 // 声明变量
@@ -12,40 +12,40 @@ var c, python, java bool
 
 // 基础语法
 func main() {
-	fmt.Println("hello world")
-	fmt.Printf("time: %s \n", time.Now().String())
+    fmt.Println("hello world")
+    fmt.Printf("time: %s \n", time.Now().String())
 
-	fmt.Println("random number: ", rand.Intn(10))
+    fmt.Println("random number: ", rand.Intn(10))
 
-	fmt.Println(math.Pi)
+    fmt.Println(math.Pi)
 
-	fmt.Println(add(1, 9))
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+    fmt.Println(add(1, 9))
+    a, b := swap("hello", "world")
+    fmt.Println(a, b)
 
-	fmt.Println(split(18))
+    fmt.Println(split(18))
 
-	//var i, j int = 1, 2
-	var i, j = 1, 2
-	fmt.Println(i, j, c, python, java)
+    //var i, j int = 1, 2
+    var i, j = 1, 2
+    fmt.Println(i, j, c, python, java)
 
-	i, j = 1, 2
-	c, python, java := "c", true, 1
-	fmt.Println(i, j, c, python, java)
+    i, j = 1, 2
+    c, python, java := "c", true, 1
+    fmt.Println(i, j, c, python, java)
 }
 
 func add(x int, y int) int {
-	return x + y
+    return x + y
 }
 
 // 支持多个返回值
 func swap(x, y string) (string, string) {
-	return y, x
+    return y, x
 }
 
 // 直接返回已命名返回值
 func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
+    x = sum * 4 / 9
+    y = sum - x
+    return
 }
