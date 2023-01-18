66ec53eea65ec1893e647194c5c2bfc864d0b933
haifuns <haifuns@163.com>
2023-01-18 18:52:36 +0800

    method

---
+++ 03_method/01_basic_method/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+// Distance 普通函数
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// Distance Point独占方法
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func main() {
+	p := Point{3, 4}
+	q := Point{6, 8}
+	fmt.Println(p.Distance(q))
+	fmt.Println(Distance(p, q))
+}
